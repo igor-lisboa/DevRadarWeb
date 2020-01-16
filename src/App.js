938459d41d73f26a9a3dc0890ba1a4e4e12ce158
Igor Lisboa <igor.lisboa@icraft.com.br>
2020-01-15 22:05:05 -0300

    utilizando estado no react pegando variavel e funcao p setar variavel

--- src/App.js
+++ src/App.js
@@ -1,17 +1,19 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 
 // Componente: Bloco isolado de HTML, CSS e JS; o qual não interfere no restante da aplicação
 // Propriedade: Informações que um componente pai passa para o componente filho
 // Estado: 
 function App() {
+    const [counter, setCounter] = useState(0);
+
     function incrementCounter() {
-        alert('oi');
+        setCounter(counter + 1);
     }
 
     return (
         <>
-            <h1>Contador: 0</h1>
+            <h1>Contador: {counter}</h1>
             <button onClick={incrementCounter}>Incrementar</button>
         </>
     );
