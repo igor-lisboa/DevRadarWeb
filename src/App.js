a20325a005e3586469aab1e8520df3e41abf24d3
Igor Lisboa <igor.lisboa@icraft.com.br>
2020-01-15 23:22:58 -0300

    tratando variaveis do form e recuperando localizacao

--- src/App.js
+++ src/App.js
@@ -1,38 +1,89 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 import './global.css';
 import './App.css';
 import './Sidebar.css';
 import './Main.css';
 
-
 function App() {
+    const [latitude, setLatitude] = useState('');
+    const [longitude, setLongitude] = useState('');
+    const [github_username, setGitubUsername] = useState('');
+    const [techs, setTechs] = useState('');
+
+
+    // useEffect => RECEBE 2 PARAMETROS, primeiro eh a funcao a ser executada, segundo eh array de variaveis q ao mudarem fazem a funcao executar
+    useEffect(() => {
+        navigator.geolocation.getCurrentPosition(
+            (position) => {
+                const { latitude, longitude } = position.coords;
+                setLatitude(latitude);
+                setLongitude(longitude);
+            },
+            (err) => {
+                console.log(err);
+            },
+            {
+                timeout: 30000,
+            }
+        )
+    }, []);
+
+    async function handleAddDev(e) {
+
+    }
 
     return (
         <>
             <div id="app">
                 <aside>
                     <strong>Cadastrar</strong>
-                    <form>
+                    <form onSubmit={handleAddDev}>
                         <div className="input-block">
                             <label htmlFor="github_username">Usuário do Github</label>
-                            <input name="github_username" id="github_username" required />
+                            <input
+                                name="github_username"
+                                id="github_username"
+                                value={github_username}
+                                onChange={e => setGitubUsername(e.target.value)}
+                                required
+                            />
                         </div>
 
                         <div className="input-block">
                             <label htmlFor="techs">Tecnologias</label>
-                            <input name="techs" id="techs" required />
+                            <input
+                                name="techs"
+                                id="techs"
+                                value={techs}
+                                onChange={e => setTechs(e.target.value)}
+                                required
+                            />
                         </div>
 
                         <div className="input-group">
                             <div className="input-block">
                                 <label htmlFor="latitude">Latitude</label>
-                                <input name="latitude" id="latitude" required />
+                                <input
+                                    type="number"
+                                    name="latitude"
+                                    id="latitude"
+                                    value={latitude}
+                                    onChange={e => setLatitude(e.target.value)}
+                                    required
+                                />
                             </div>
 
                             <div className="input-block">
                                 <label htmlFor="longitude">Longitude</label>
-                                <input name="longitude" id="longitude" required />
+                                <input
+                                    type="number"
+                                    name="longitude"
+                                    id="longitude"
+                                    value={longitude}
+                                    onChange={e => setLongitude(e.target.value)}
+                                    required
+                                />
                             </div>
                         </div>
 
@@ -41,6 +92,39 @@ function App() {
                 </aside>
                 <main>
                     <ul>
+                        <li className="dev-item">
+                            <header>
+                                <img src="https://avatars3.githubusercontent.com/u/45468193?s=460&v=4" alt="Igor" />
+                                <div className="user-info">
+                                    <strong>Igor Lisboa</strong>
+                                    <span>JavaScript, Java, TypeScript, REACT</span>
+                                </div>
+                            </header>
+                            <p>Sempre caçando novas linguagens, frameworks e tentando evoluir como programador (back-end | front-end | dba | mobile)</p>
+                            <a href="https://github.com/igor-lisboa">Acessar perfil no github</a>
+                        </li>
+                        <li className="dev-item">
+                            <header>
+                                <img src="https://avatars3.githubusercontent.com/u/45468193?s=460&v=4" alt="Igor" />
+                                <div className="user-info">
+                                    <strong>Igor Lisboa</strong>
+                                    <span>JavaScript, Java, TypeScript, REACT</span>
+                                </div>
+                            </header>
+                            <p>Sempre caçando novas linguagens, frameworks e tentando evoluir como programador (back-end | front-end | dba | mobile)</p>
+                            <a href="https://github.com/igor-lisboa">Acessar perfil no github</a>
+                        </li>
+                        <li className="dev-item">
+                            <header>
+                                <img src="https://avatars3.githubusercontent.com/u/45468193?s=460&v=4" alt="Igor" />
+                                <div className="user-info">
+                                    <strong>Igor Lisboa</strong>
+                                    <span>JavaScript, Java, TypeScript, REACT</span>
+                                </div>
+                            </header>
+                            <p>Sempre caçando novas linguagens, frameworks e tentando evoluir como programador (back-end | front-end | dba | mobile)</p>
+                            <a href="https://github.com/igor-lisboa">Acessar perfil no github</a>
+                        </li>
                         <li className="dev-item">
                             <header>
                                 <img src="https://avatars3.githubusercontent.com/u/45468193?s=460&v=4" alt="Igor" />
