1f0fcc4f7b4b879c686daa3c3fa41c7ddcbe4006
Igor Lisboa <igor.lisboa@icraft.com.br>
2020-01-21 23:58:58 -0300

    normalizando proj

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import api from './services/api';
 
 import './global.css';
@@ -6,45 +6,44 @@ import './App.css';
 import './Sidebar.css';
 import './Main.css';
 
-import DevItem from './components/DevItem';
 import DevForm from './components/DevForm';
+import DevItem from './components/DevItem';
 
 function App() {
-    const [devs, setDevs] = useState([]);
+  const [devs, setDevs] = useState([]);
 
-    useEffect(() => {
-        async function loadDevs() {
-            const response = await api.get('/devs');
+  useEffect(() => {
+    async function loadDevs() {
+      const response = await api.get('/devs');
 
-            setDevs(response.data);
-        }
-
-        loadDevs();
-    }, []);
-
-    async function handleAddDev(data) {
-        const response = await api.post('/devs', data);
-
-        setDevs([...devs, response.data]); // pega devs e adiciona response.data
+      setDevs(response.data);
     }
 
-    return (
-        <>
-            <div id="app">
-                <aside>
-                    <strong>Cadastrar</strong>
-                    <DevForm onSubmit={handleAddDev} />
-                </aside>
-                <main>
-                    <ul>
-                        {devs.map(dev => (
-                            <DevItem key={dev._id} dev={dev} />
-                        ))}
-                    </ul>
-                </main>
-            </div>
-        </>
-    );
+    loadDevs();
+  }, []);
+
+  async function handleAddDev(data) {
+    const response = await api.post('/devs', data)
+
+    setDevs([...devs, response.data]);
+  }
+
+  return (
+    <div id="app">
+      <aside>
+        <strong>Cadastrar</strong>
+        <DevForm onSubmit={handleAddDev} />
+      </aside>
+
+      <main>
+        <ul>
+          {devs.map(dev => (
+            <DevItem key={dev._id} dev={dev} />
+          ))}
+        </ul>
+      </main>
+    </div>
+  );
 }
 
 export default App;
--- src/components/DevForm/index.js
+++ src/components/DevForm/index.js
@@ -1,90 +1,95 @@
 import React, { useState, useEffect } from 'react';
 
 function DevForm({ onSubmit }) {
-    const [latitude, setLatitude] = useState('');
-    const [longitude, setLongitude] = useState('');
-    const [github_username, setGitubUsername] = useState('');
-    const [techs, setTechs] = useState('');
+  const [github_username, setGithubUsername] = useState('');
+  const [techs, setTechs] = useState('');
+  const [latitude, setLatitude] = useState('');
+  const [longitude, setLongitude] = useState('');
 
-    // useEffect => RECEBE 2 PARAMETROS, primeiro eh a funcao a ser executada, segundo eh array de variaveis q ao mudarem fazem a funcao executar
-    useEffect(() => {
-        navigator.geolocation.getCurrentPosition(
-            (position) => {
-                const { latitude, longitude } = position.coords;
-                setLatitude(latitude);
-                setLongitude(longitude);
-            },
-            (err) => {
-                console.log(err);
-            },
-            {
-                timeout: 30000,
-            }
-        )
-    }, []);
+  useEffect(() => {
+    navigator.geolocation.getCurrentPosition(
+      (position) => {
+        const { latitude, longitude } = position.coords;
 
-    async function handleSubmit(e) {
-        e.preventDefault();
+        setLatitude(latitude);
+        setLongitude(longitude);
+      },
+      (err) => {
+        console.log(err);
+      },
+      {
+        timeout: 30000,
+      }
+    )
+  }, []);
 
-        await onSubmit({ github_username, techs, latitude, longitude });
+  async function handleSubmit(e) {
+    e.preventDefault();
 
-        setGitubUsername('');
-        setTechs('');
-    }
+    await onSubmit({
+      github_username,
+      techs,
+      latitude,
+      longitude,
+    });
 
-    return (
-        <form onSubmit={handleSubmit}>
-            <div className="input-block">
-                <label htmlFor="github_username">Usuário do Github</label>
-                <input
-                    name="github_username"
-                    id="github_username"
-                    value={github_username}
-                    onChange={e => setGitubUsername(e.target.value)}
-                    required
-                />
-            </div>
+    setGithubUsername('');
+    setTechs('');
+  }
 
-            <div className="input-block">
-                <label htmlFor="techs">Tecnologias</label>
-                <input
-                    name="techs"
-                    id="techs"
-                    value={techs}
-                    onChange={e => setTechs(e.target.value)}
-                    required
-                />
-            </div>
+  return (
+    <form onSubmit={handleSubmit}>
+      <div className="input-block">
+        <label htmlFor="github_username">Usuário do Github</label>
+        <input 
+          name="github_username" 
+          id="github_username" 
+          required
+          value={github_username}
+          onChange={e => setGithubUsername(e.target.value)}
+        />
+      </div>
 
-            <div className="input-group">
-                <div className="input-block">
-                    <label htmlFor="latitude">Latitude</label>
-                    <input
-                        type="number"
-                        name="latitude"
-                        id="latitude"
-                        value={latitude}
-                        onChange={e => setLatitude(e.target.value)}
-                        required
-                    />
-                </div>
+      <div className="input-block">
+        <label htmlFor="techs">Tecnologias</label>
+        <input
+          name="techs"
+          id="techs"
+          required
+          value={techs}
+          onChange={e => setTechs(e.target.value)}
+        />
+      </div>
 
-                <div className="input-block">
-                    <label htmlFor="longitude">Longitude</label>
-                    <input
-                        type="number"
-                        name="longitude"
-                        id="longitude"
-                        value={longitude}
-                        onChange={e => setLongitude(e.target.value)}
-                        required
-                    />
-                </div>
-            </div>
+      <div className="input-group">
+        <div className="input-block">
+          <label htmlFor="latitude">Latitude</label>
+          <input 
+            type="number" 
+            name="latitude" 
+            id="latitude" 
+            required 
+            value={latitude}
+            onChange={e => setLatitude(e.target.value)}
+          />
+        </div>
 
-            <button type="submit">Salvar</button>
-        </form>
-    );
+        <div className="input-block">
+          <label htmlFor="longitude">Longitude</label>
+          <input
+            type="number"
+            name="longitude"
+            id="longitude"
+            required
+            value={longitude}
+            onChange={e => setLongitude(e.target.value)}
+          />
+        </div>
+      </div>
+
+      <button type="submit">Salvar</button>
+    </form>
+  );
 }
 
 export default DevForm;
